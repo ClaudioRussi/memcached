0a367634230ba2cc194215861b528407a840a75a
Claudio Russi <=>
2019-10-18 17:49:21 -0300

    Add test classes

---
+++ memcached_test.rb
@@ -0,0 +1,6 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
+class MemcachedTest < Test::Unit::TestCase
+    
+end
---
+++ value_test.rb
@@ -0,0 +1,6 @@
+require "test/unit/assertions"
+include Test::Unit::Assertions
+
+class ValueTest < Test::Unit::TestCase
+
+end
